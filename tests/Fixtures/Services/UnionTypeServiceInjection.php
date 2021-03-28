d0f4f435ef3e9a9100d12ccb68f2c1604723cc6e
Alexandre Gérault <alexandre@hexium.io>
2021-03-28 18:30:59 +0200

    style(container): apply PSR12 code style on test files

--- tests/Fixtures/Services/UnionTypeServiceInjection.php
+++ tests/Fixtures/Services/UnionTypeServiceInjection.php
@@ -1,19 +1,17 @@
 <?php
 
-
 namespace Test\Fixtures\Services;
-
 
 class UnionTypeServiceInjection
 {
-    protected FooService|FooAltService $service;
+    protected FooService | FooAltService $service;
 
     /**
      * UnionTypeServiceInjection constructor.
      *
      * @param FooAltService|FooService $service
      */
-    public function __construct(FooService|FooAltService $service)
+    public function __construct(FooService | FooAltService $service)
     {
         $this->service = $service;
     }
