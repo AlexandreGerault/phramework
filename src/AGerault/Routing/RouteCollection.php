22b2cee23feebde59b9def5c68e1f3388cbe9f59
Alexandre Gérault <alexandre@hexium.io>
2021-04-20 09:11:57 +0200

    feat(routing): route collection class

--- src/AGerault/Routing/RouteCollection.php
+++ src/AGerault/Routing/RouteCollection.php
@@ -4,7 +4,23 @@
 namespace AGerault\Framework\Routing;
 
 
-class RouteCollection
-{
+use AGerault\Framework\Contracts\Routing\RouteCollectionInterface;
+use AGerault\Framework\Contracts\Routing\RouteInterface;
 
+class RouteCollection implements RouteCollectionInterface
+{
+    /**
+     * @var array<RouteInterface>
+     */
+    protected array $routes = [];
+
+    public function registerRoute(RouteInterface $route): void
+    {
+        $this->routes[] = $route;
+    }
+
+    public function routes(): array
+    {
+        return $this->routes;
+    }
 }
