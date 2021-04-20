a7bf34554f9a851c85d3108e672ed9530447dbc6
Alexandre Gérault <alexandre@hexium.io>
2021-04-20 09:11:57 +0200

    feat(routing): route collection class

--- tests/Fixtures/Routing/MockedRoute.php
+++ tests/Fixtures/Routing/MockedRoute.php
@@ -1,10 +1,46 @@
 <?php
 
-
 namespace Test\Fixtures\Routing;
 
+use AGerault\Framework\Contracts\Routing\RouteInterface;
 
-class MockedRoute
+class MockedRoute implements RouteInterface
 {
 
+    public function name(): string
+    {
+    }
+
+    public function callback(): callable
+    {
+        return function () {};
+    }
+
+    public function method(): string
+    {
+        return '';
+    }
+
+    public function parameter(string $id): mixed
+    {
+    }
+
+    public function parameterNames(): array
+    {
+        return [];
+    }
+
+    public function url(): string
+    {
+        return "";
+    }
+
+    public function parameters(): array
+    {
+        return [];
+    }
+
+    public function setParameter(string $parameterName, mixed $value): void
+    {
+    }
 }
--- tests/Unit/Routing/RouteCollectionTest.php
+++ tests/Unit/Routing/RouteCollectionTest.php
@@ -1,1 +1,16 @@
 <?php
+
+use AGerault\Framework\Routing\RouteCollection;
+use Test\Fixtures\Routing\MockedRoute;
+
+it('should increase the routes number after registering one',function() {
+    $collection = new RouteCollection();
+    $route = new MockedRoute();
+
+    $collection->registerRoute($route);
+
+    expect($collection->routes())
+        ->toBeArray()
+        ->toContain($route)
+        ->toHaveCount(1);
+});
