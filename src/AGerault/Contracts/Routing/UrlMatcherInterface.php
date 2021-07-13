c65f948f151d029d68e91453c49d6c3a293aba17
Alexandre Gérault <alexandre@hexium.io>
2021-07-13 14:15:52 +0200

    feat(router): add route not found execption in phpdoc

--- src/AGerault/Contracts/Routing/UrlMatcherInterface.php
+++ src/AGerault/Contracts/Routing/UrlMatcherInterface.php
@@ -2,7 +2,15 @@
 
 namespace AGerault\Framework\Contracts\Routing;
 
+use AGerault\Framework\Routing\Exceptions\RouteNotFoundException;
+
 interface UrlMatcherInterface
 {
+    /**
+     * @param string $uri
+     * @param string $method
+     * @return RouteInterface
+     * @throws RouteNotFoundException
+     */
     public function match(string $uri, string $method): RouteInterface;
 }
