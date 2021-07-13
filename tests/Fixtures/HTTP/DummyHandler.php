7b5b51d8a43b383c5d1fe41c6a4ad6292d4336e4
Alexandre Gérault <alexandre@hexium.io>
2021-07-13 14:56:27 +0200

    test(http): dummy handler to validate http method middleware

--- tests/Fixtures/HTTP/DummyHandler.php
+++ tests/Fixtures/HTTP/DummyHandler.php
@@ -11,8 +11,18 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class DummyHandler implements HttpRequestHandlerInterface
 {
+    protected ServerRequestInterface $request;
+
+    public function __construct() {}
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $this->request = $request;
         return new Response(200, [], "Response");
+    }
+
+    public function request(): ServerRequestInterface
+    {
+        return $this->request;
     }
 }
